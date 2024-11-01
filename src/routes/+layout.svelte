a6699d913009613a9ca01b1316c424a3d23996bf
Mateo Nares <matthew_nares@apple.com>
2024-11-01 12:30:29 -0700

    initial commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
-<script lang="ts">
-	import '../app.css';
+<script>
 	let { children } = $props();
 </script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/blog">blog</a>
+</nav>
 
 {@render children()}
